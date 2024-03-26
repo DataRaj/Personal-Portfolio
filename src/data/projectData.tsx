1b183e0b458caede7c419183b1e6dd0a7db9509e
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-03-26 23:58:44 +0530

    Update projectData.tsx

--- src/data/projectData.tsx
+++ src/data/projectData.tsx
@@ -209,33 +209,4 @@ export const projectData: ProjectT[] = [
     //     ],
     //     headerLink: 'https://github.com/jasonfyw/csc258-project',
     // },
-    {
-        title: 'Design Intern',
-        company: 'DevStack Solution',
-        location: 'In, (Remote)',
-        dates: 'Feb 2022 - May 2022',
-        description:
-            'Developed and Tested a various types of front-end components and debugged complex hooks and , Learned a lot of things eg: Development process of large scale applications and developed projects of cloning highly used applications using modern technologies ,TailwindCss, ReactJS, TypeScript, and AWS Lambda.',
-        logoSrc:
-            'https://media.licdn.com/dms/image/C560BAQELxHhRsNPnjA/company-logo_100_100/0/1630644452398/devstack_solutions_logo?e=1717632000&v=beta&t=nM0wb7d11qZ-VpRJZvECOzlknt2kot4ZqJ_6OiqqiJ0',
-    },
-    {
-        title: 'Intern',
-        company: 'MICA Labs',
-        location: 'Maharashtra, IN',
-        dates: 'May 2019 - July 2019',
-        description:
-            'Specialized in MCB Login with PLC and SCADA for more about automations and core industry functions, Then on other half time directed toward Web developement, like HTML, CSS and Javascript basics for covering all the fundamentals ',
-        logoSrc:
-            'https://media.licdn.com/dms/image/C5103AQF7HJ61WxvX_Q/profile-displayphoto-shrink_800_800/0/1581501087928?e=1715212800&v=beta&t=O2aqG6C9IaGE-CpzH9FQQFdAwopgIdIRS_9seE-gSKk',
-    },
-    {
-        title: 'Backend Developer',
-        company: 'Bharat IQ',
-        location: 'In, (Remote)',
-        dates: 'Jun 2023 - August 2023',
-        description: 
-            'Bharat IQ was fully based on the backend specialization with Java, I developed a program to improve the efficiency of the code by using core java tools like lamda expression, sealed classes and records. Utlizied Spring JPA and Hibernate, optmizing database connectivity by 30%. Server managed by CRUD operations',
-        logoSrc : 'https://media.licdn.com/dms/image/D4D0BAQE5gA28bRqA7g/company-logo_100_100/0/1687453214135/coder_point_technology_logo?e=1719446400&v=beta&t=2ovzqLPPuWL5iVxWsqQX-8P5tceO1dP35JL4PfXa3co'
-    }
 ]
