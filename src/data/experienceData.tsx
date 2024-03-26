b4124ca7d3ee8fa49d9e57d7796bbe3452f8b7ce
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-03-26 23:57:54 +0530

    Update experienceData.tsx

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -21,4 +21,13 @@ export const experienceData: ExperienceData[] = [
         logoSrc:
             'https://media.licdn.com/dms/image/C5103AQF7HJ61WxvX_Q/profile-displayphoto-shrink_800_800/0/1581501087928?e=1715212800&v=beta&t=O2aqG6C9IaGE-CpzH9FQQFdAwopgIdIRS_9seE-gSKk',
     },
+        {
+        title: 'Backend Developer',
+        company: 'Bharat IQ',
+        location: 'In, (Remote)',
+        dates: 'Jun 2023 - August 2023',
+        description: 
+            'Bharat IQ was fully based on the backend specialization with Java, I developed a program to improve the efficiency of the code by using core java tools like lamda expression, sealed classes and records. Utlizied Spring JPA and Hibernate, optmizing database connectivity by 30%. Server managed by CRUD operations',
+        logoSrc : 'https://media.licdn.com/dms/image/D4D0BAQE5gA28bRqA7g/company-logo_100_100/0/1687453214135/coder_point_technology_logo?e=1719446400&v=beta&t=2ovzqLPPuWL5iVxWsqQX-8P5tceO1dP35JL4PfXa3co'
+    }
 ]
