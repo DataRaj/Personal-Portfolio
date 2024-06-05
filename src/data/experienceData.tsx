8175502082b7084f48c7ecb25918227599151fde
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-06-05 00:49:19 -0400

    Update experienceData.tsx

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -5,25 +5,23 @@ export const experienceData: ExperienceData[] = [
         company: 'MICA Labs',
         location: 'Maharashtra, IN',
         dates: 'May 2019 - July 2019',
-        description:
-            'During my internship, I utilized Node.js, EJS, Bootstrap, and PHP to develop a comprehensive software project. I successfully generated backend data and created a frontend with 25% growth. I completed a time-bound task by building a software project with both front-end and back-end services, demonstrating my mastery of JavaScript, asynchronous programming, and design patterns.',    
+        description:'During my internship, I utilized Node.js, EJS, Bootstrap, and PHP to develop a comprehensive software project. I successfully generated backend data and created a frontend with 25% growth. I completed a time-bound task by building a software project with both front-end and back-end services, demonstrating my mastery of JavaScript, asynchronous programming, and design patterns.',    
         logoSrc:'/micalabs.jpg',
     },
     {
         title: 'Design Intern',
         company: 'DevStack Solution',
-        location: 'In, (Remote)',
+        location: 'IN, (Remote)',
         dates: 'Feb 2022 - May 2022',
-        description:
-            'I devstack, I worked on crucial continueous development enviroment, I developed and tested various front-end components, debugging complex hooks. I gained expertise in large-scale application development and cloned popular applications using modern technologies like TailwindCSS, ReactJS, TypeScript, and AWS Lambda.',
+        description:'I devstack, I worked on crucial continueous development enviroment, I contributed significantly to UI design, enhancing interaction and accelerating project delivery. I designed complex React components using TypeScript, custom hooks, and optimizations, and effectively managed state with Redux, handling numerous states efficiently.',
         logoSrc:'/devstack.jpg',
     },
         {
         title: 'Backend Developer',
         company: 'Bharat IQ',
-        location: 'In, (Remote)',
-        dates: 'Jun 2023 - August 2023',
-        description: 'During my internship at Bharat IQ, I focused on the back-end, utilizing NodeJS, Express, and Prisma tools like Prisma ORM, TypeORM, sealed classes, and records. I optimized database connectivity by 30% using GraphQL and Apollo, and managed servers through CRUD operations, significantly improving application efficiency.',
+        location: 'IN, (Remote)',
+        dates: 'July 2023 - Sept 2023',
+        description: 'During my internship, I contributed to the CRM tool development team, enhancing application performance and reducing bugs by 30%. I configured a scalable backend with multi-layered APIs, payment, mailing, and authentication integrations. I assisted in testing the database using mock data and gained valuable insights from senior developers.',
         logoSrc : '/bharatiq.jpg'
     }
 ]
