3ec99eb5c0f5d2cca490db55b95f1791db0074d5
Dattaraj Dhadas <87413639+DataRaj@users.noreply.github.com>
2024-09-16 05:24:48 -0400

    Update projectData.tsx

--- src/data/projectData.tsx
+++ src/data/projectData.tsx
@@ -43,7 +43,7 @@ export const projectData: ProjectT[] = [
                 icon: <FiGithub />,
             },
         ],
-        headerLink: 'https://d-socials-media-basedweb-m9yzk1v8n-datarajs-projects.vercel.app/signin' ,
+        headerLink: 'https://d-socials-media-basedweb-m9yzk1v8n-datarajs-projects.vercel.app' ,
     },
     {
         name: 'Food-Deli-App',
