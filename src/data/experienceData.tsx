b23baaeff7b9de1ec28428e5b2466275bda68969
Dattaraj Dhadas <dhadas808@yahoo.com>
2025-01-28 13:26:48 +0530

    update

--- src/data/experienceData.tsx
+++ src/data/experienceData.tsx
@@ -5,7 +5,10 @@ export const experienceData: ExperienceData[] = [
         company: 'MICA Labs',
         location: 'Maharashtra, IN',
         dates: 'May 2019 - July 2019',
-        description:'During my internship, I utilized Node.js, EJS, Bootstrap, and PHP to develop a comprehensive software project. I successfully generated backend data and created a frontend with 25% growth. I completed a time-bound task by building a software project with both front-end and back-end services, demonstrating my mastery of JavaScript, asynchronous programming, and design patterns.',    
+        description:`During my internship, I utilized Node.js, EJS, Bootstrap, and PHP to develop a comprehensive 
+                     software project. I successfully generated backend data and created a frontend with 25% growth. I 
+                     completed a time-bound task by building a software project with both front-end and back-end services,
+                     demonstrating my mastery of JavaScript, asynchronous programming, and design patterns.`,    
         logoSrc:'/micalabs.jpg',
     },
     {
@@ -13,7 +16,10 @@ export const experienceData: ExperienceData[] = [
         company: 'DevStack Solution',
         location: 'IN, (Remote)',
         dates: 'Feb 2022 - May 2022',
-        description:'I devstack, I worked on crucial continueous development enviroment, I contributed significantly to UI design, enhancing interaction and accelerating project delivery. I designed complex React components using TypeScript, custom hooks, and optimizations, and effectively managed state with Redux, handling numerous states efficiently.',
+        description:`I devstack, I worked on crucial continueous development enviroment, I contributed significantly to 
+                     UI design, enhancing interaction and accelerating project delivery. I designed complex React 
+                     components using TypeScript, custom hooks, and optimizations, and effectively managed state with 
+                     Redux, handling numerous states efficiently.`,
         logoSrc:'/devstack.jpg',
     },
         {
@@ -21,15 +27,24 @@ export const experienceData: ExperienceData[] = [
         company: 'Bharat IQ',
         location: 'IN, (Remote)',
         dates: 'July 2023 - Sept 2023',
-        description: 'During my internship, I contributed to the CRM tool development team, enhancing application performance and reducing bugs by 30%. I configured a scalable backend with multi-layered APIs, payment, mailing, and authentication integrations. I assisted in testing the database using mock data and gained valuable insights from senior developers.',
+        description: `During my internship, I contributed to the CRM tool development team, enhancing application 
+                    performance and reducing bugs by 30%. I configured a scalable backend with multi-layered APIs, 
+                    payment, mailing, and authentication integrations. I assisted in testing the database using mock 
+                    data and gained valuable insights from senior developers.`,
         logoSrc : '/bharatiq.jpg'
-    }, 
+    },
     {
-        title: 'Full Stack Developer',
-        company: 'Shiven Infotech',
-        location: 'Navi Mumbai, IN',
-        dates: 'Jun 2024 - Aug 2024',
-        description: 'Shiven InfoTech is new tech startup which farm based product selling and software based service small firm, I worked as a Software Developed. Developed a Ecommerce Web-App using NextJs, Express, SanityIo, Drizzle Orm, PostgreSQL, Redis, Websockets and Auth system. I also Worked on more than 5 webpages which comprises landing pages, card service app. I solely developed everything',
-        logoSrc : '/shiven.jpg'
+        title: "Software Developer",
+        company: "Dhadas Fintech",
+        location: "Mumbai, IN",
+        dates: "Oct 2023 - Sept 2024",
+        description: `I worked as a Software Developer in Dhadas Fintech, which is a fintech small firm company leading
+                      to work with clients to provide serivces such loans and software tools through third party businesses. I worked on 
+                      Developed and optimized frontend applications using Angular, leveraging modern concepts like reactive
+                      forms, lazy loading, and module splitting for improved performance and scalability. worked 
+                      caching with using modern Angular advanced features, ensuring seamless user experiences with state
+                      management and responsive design. Exclusively worked on database structure and data flow using 
+                      Java and SQL, using Maven and Hibernate libraries pairing Spring for faster and seamless integration with Database and client side.`,
+        logoSrc: '/dhadas.jpg'
     }
 ]
